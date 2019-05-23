7057cf6c61b3c90ec3d6aa131ae7de70c0655e54
Farzad Abdolhosseini <farzad.abdolhosseini@gmail.com>
2019-05-23 10:46:56 -0700

    Dockerfile + run script adapted for singularity

--- scripts/setup_experiment.sh
+++ scripts/setup_experiment.sh
@@ -29,12 +29,14 @@ cat > $log_path/run_script.sh <<EOF
 #SBATCH --cpus-per-task=$num_procs
 #SBATCH --mem-per-cpu=400M
 #SBATCH --array=1-$num_replicates
-source $HOME/cproj/venv/bin/activate
-cd `pwd`
-python -m main --log_dir $log_path/\$SLURM_ARRAY_TASK_ID \
+module load singularity/3.2
+cd `pwd`    # run this from the walking-benchmark folder
+cd ..       # go back to the parent dir, i.e. cproj
+command="python -m main --log_dir $log_path/\$SLURM_ARRAY_TASK_ID \
     --seed \$SLURM_ARRAY_TASK_ID \
     --load_path $log_path \
-    --num_processes $num_procs --name $name--run\$SLURM_ARRAY_TASK_ID
+    --num_processes $num_procs --name $name--run\$SLURM_ARRAY_TASK_ID"
+singularity exec -B /home -B /project -B /scratch imgwalking.sif bash -c "cd walking-benchmark && $command"
 EOF
 
 cd $log_path
